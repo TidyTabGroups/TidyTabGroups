37ad2f89d40ad2c1f7f0c04ebdb37f5cf85ad0b2
Moe Bazzi <bazzimoe3@gmail.com>
2024-02-14 17:22:24 +0000

    added some active window themes

---
+++ src/utils/themes/index.ts
@@ -0,0 +1,21 @@
+export const themes: {
+  name: string;
+  primaryColor: chrome.tabGroups.ColorEnum;
+  secondaryColor: chrome.tabGroups.ColorEnum;
+}[] = [
+  {
+    name: "purple-yellow",
+    primaryColor: "purple",
+    secondaryColor: "yellow",
+  },
+  {
+    name: "blue-green",
+    primaryColor: "green",
+    secondaryColor: "blue",
+  },
+  {
+    name: "pink-purple",
+    primaryColor: "purple",
+    secondaryColor: "pink",
+  },
+];
