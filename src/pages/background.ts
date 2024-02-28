d7ee5c1b36c9a202ffac963a07ab0ae1e062b692
Moe Bazzi <bazzimoe3@gmail.com>
2024-02-28 14:30:22 +0000

    move chrome extension pages files to src/pages/

--- src/pages/background.ts
+++ src/pages/background.ts
@@ -1,5 +1,5 @@
-import BackgroundEvents from "./backgroundEvents";
-import Database from "./database";
+import BackgroundEvents from "../backgroundEvents";
+import Database from "../database";
 (async function main() {
   chrome.alarms.onAlarm.addListener(BackgroundEvents.onAlarm);
   chrome.runtime.onInstalled.addListener(BackgroundEvents.onInstalled);
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,13 +2,14 @@ const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
+const srcPagesDir = path.join(srcDir, "pages");
 
 module.exports = {
     entry: {
-      popup: path.join(srcDir, 'popup.tsx'),
-      options: path.join(srcDir, 'options.tsx'),
-      background: path.join(srcDir, 'background.ts'),
-      content_script: path.join(srcDir, 'content_script.tsx'),
+      popup: path.join(srcPagesDir, 'popup.tsx'),
+      options: path.join(srcPagesDir, 'options.tsx'),
+      background: path.join(srcPagesDir, 'background.ts'),
+      content_script: path.join(srcPagesDir, 'content_script.tsx'),
     },
     output: {
         path: path.join(__dirname, "../dist/js"),
