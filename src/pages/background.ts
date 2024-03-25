5e2ec1d624cab504b2fb751876d961366949fda2
Moe Bazzi <bazzimoe3@gmail.com>
2024-03-25 18:43:18 +0000

    implement background event queue

--- src/pages/background.ts
+++ src/pages/background.ts
@@ -1,20 +1,6 @@
 import BackgroundEvents from "../backgroundEvents";
 import Database from "../database";
 (async function main() {
-  chrome.runtime.onInstalled.addListener(BackgroundEvents.onInstalled);
-  chrome.runtime.onMessage.addListener(BackgroundEvents.onMessage);
-
-  chrome.windows.onCreated.addListener(BackgroundEvents.onWindowCreated);
-  chrome.windows.onRemoved.addListener(BackgroundEvents.onWindowRemoved);
-
-  chrome.tabs.onCreated.addListener(BackgroundEvents.onTabCreated);
-  chrome.tabs.onActivated.addListener(BackgroundEvents.onTabActivated);
-  chrome.tabs.onUpdated.addListener(BackgroundEvents.onTabUpdated);
-  chrome.tabs.onRemoved.addListener(BackgroundEvents.onTabRemoved);
-  chrome.tabs.onMoved.addListener(BackgroundEvents.onTabMoved);
-  chrome.tabs.onReplaced.addListener(BackgroundEvents.onTabReplaced);
-
-  chrome.tabGroups.onUpdated.addListener(BackgroundEvents.onTabGroupsUpdated);
-
   Database.initializeDatabaseConnection("model");
+  BackgroundEvents.initialize();
 })();
