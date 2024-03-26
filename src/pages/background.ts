841102bcf6099222b6bab03681b92fa6cf885d48
Moe Bazzi <bazzimoe3@gmail.com>
2024-03-26 18:51:39 +0000

    handle errors and timeouts in backround event operation queue, and bubble them up to the background page

--- src/pages/background.ts
+++ src/pages/background.ts
@@ -1,6 +1,15 @@
 import BackgroundEvents from "../backgroundEvents";
 import Database from "../database";
+import Logger from "../logger";
+
+const logger = Logger.getLogger("Background", { color: "pink" });
+
 (async function main() {
   Database.initializeDatabaseConnection("model");
-  BackgroundEvents.initialize();
+  BackgroundEvents.initialize(onError);
 })();
+
+async function onError() {
+  logger.error("onError::An error occurred in the background page. Will try to recover...");
+  chrome.runtime.reload();
+}
