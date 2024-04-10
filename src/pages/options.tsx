8f4a7e80be4212aa1bffe8c1c26c97508dadda58
Moe Bazzi <bazzimoe3@gmail.com>
2024-04-10 18:30:20 +0100

    Add UI and storage for user settings

--- src/pages/options.tsx
+++ src/pages/options.tsx
@@ -1,70 +1,174 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import * as Storage from "../storage";
+import { UserSettings } from "../types/types";
+import {
+  Switch,
+  Container,
+  Divider,
+  createTheme,
+  ThemeProvider,
+  useMediaQuery,
+  Card,
+  Typography,
+  CssBaseline,
+  CardContent,
+  AppBar,
+  Toolbar,
+  Grid,
+  Input,
+  Slider,
+} from "@mui/material";
 
-const Options = () => {
-  const [color, setColor] = useState<string>("");
-  const [status, setStatus] = useState<string>("");
-  const [like, setLike] = useState<boolean>(false);
+const UserSettings = () => {
+  const [loadingSettings, setLoadingSettings] = useState(true);
+  const [userSettings, setUserSettings] = useState<UserSettings | null>(null);
+
+  const isLoading = userSettings === null;
 
   useEffect(() => {
-    // Restores select box and checkbox state using the preferences
-    // stored in chrome.storage.
-    chrome.storage.sync.get(
-      {
-        favoriteColor: "red",
-        likesColor: true,
-      },
-      (items) => {
-        setColor(items.favoriteColor);
-        setLike(items.likesColor);
+    (async () => {
+      const { userSettings } = await Storage.getItems("userSettings");
+      setLoadingSettings(false);
+      setUserSettings(userSettings);
+    })();
+
+    const subscription = Storage.changeStream.subscribe((changes) => {
+      if (changes.userSettings?.newValue) {
+        setUserSettings(changes.userSettings.newValue);
       }
-    );
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
-  const saveOptions = () => {
-    // Saves options to chrome.storage.sync.
-    chrome.storage.sync.set(
-      {
-        favoriteColor: color,
-        likesColor: like,
-      },
-      () => {
-        // Update status to let user know options were saved.
-        setStatus("Options saved.");
-        const id = setTimeout(() => {
-          setStatus("");
-        }, 1000);
-        return () => clearTimeout(id);
-      }
-    );
-  };
+  async function updateSettings(newSettings: Partial<UserSettings>) {
+    await Storage.updateItems(async (prev) => {
+      prev.userSettings = { ...prev.userSettings, ...newSettings };
+      return prev;
+    });
+  }
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
-    <>
-      <div>
-        Favorite color: <select
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        >
-          <option value="red">red</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={like}
-            onChange={(event) => setLike(event.target.checked)}
+    <Container maxWidth="md" sx={{ height: "100vh", padding: "24px" }}>
+      <Card>
+        <CardContent sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          <UserSetting
+            control={<Switch checked={userSettings.repositionTabs} onChange={(e) => updateSettings({ repositionTabs: e.target.checked })} />}
+            name="Reposition focused Tabs to the end"
           />
-          I like colors.
-        </label>
-      </div>
-      <div>{status}</div>
-      <button onClick={saveOptions}>Save</button>
-    </>
+          <Divider />
+          <UserSetting
+            control={
+              <Switch checked={userSettings.repositionTabGroups} onChange={(e) => updateSettings({ repositionTabGroups: e.target.checked })} />
+            }
+            name="Reposition focused Tab Groups to the end"
+          />
+          <Divider />
+          <UserSetting
+            control={
+              <Switch
+                checked={userSettings.addNewTabToFocusedTabGroup}
+                onChange={(e) => updateSettings({ addNewTabToFocusedTabGroup: e.target.checked })}
+              />
+            }
+            name="Automatically place new Tabs in focused Tab Group"
+          />
+          <Divider />
+          <UserSetting
+            control={
+              <Switch
+                checked={userSettings.collapseUnfocusedTabGroups}
+                onChange={(e) => updateSettings({ collapseUnfocusedTabGroups: e.target.checked })}
+              />
+            }
+            name="Automatically collapse unfocused Tab Groups"
+          />
+          <Divider />
+          <UserSetting
+            control={
+              <Switch
+                checked={userSettings.activateTabInFocusedTabGroup}
+                onChange={(e) => updateSettings({ activateTabInFocusedTabGroup: e.target.checked })}
+              />
+            }
+            name="Automatically activate Tab in focused Tab Group"
+          />
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
+
+interface UserSettingProps {
+  name: string;
+  control: React.ReactElement;
+}
+
+const UserSetting = (props: UserSettingProps) => {
+  const { name, control } = props;
+
+  return (
+    <Grid container spacing={2} alignItems="center" justifyContent="space-between">
+      <Grid item>
+        <Typography variant="h6" gutterBottom>
+          {name}
+        </Typography>
+      </Grid>
+      <Grid item xs sx={{ textAlign: "end" }}>
+        {control}
+      </Grid>
+    </Grid>
+  );
+};
+
+const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="sticky">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              TIDY TABS SETTINGS
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <UserSettings />
+    </ThemeProvider>
   );
 };
 
@@ -72,6 +176,6 @@ const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <Options />
+    <App />
   </React.StrictMode>
 );
--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,63 +1,29 @@
 import { LocalStorageShape } from "../types/types";
+import { getBucket } from "@extend-chrome/storage";
+const store = getBucket<LocalStorageShape>("store");
 
-export const LOCAL_STORAGE_DEFAULT_VALUES: LocalStorageShape = {};
+export const LOCAL_STORAGE_DEFAULT_VALUES: LocalStorageShape = {
+  userSettings: {
+    repositionTabs: true,
+    repositionTabGroups: true,
+    addNewTabToFocusedTabGroup: true,
+    collapseUnfocusedTabGroups: true,
+    activateTabInFocusedTabGroup: true,
+  },
+};
 
-export async function initialize() {
-  await setItems(LOCAL_STORAGE_DEFAULT_VALUES);
+export async function initialize(defaultValues = LOCAL_STORAGE_DEFAULT_VALUES) {
+  await setItems(defaultValues);
 }
 
-export async function getItems<T extends Partial<LocalStorageShape>>(keys?: string | string[] | { [key: string]: any } | null) {
-  return (await chrome.storage.local.get(keys)) as T;
-}
+export const getItems = store.get;
+export const setItems = store.set;
+export const clearItems = store.clear;
+export const removeItems = store.remove;
+export const updateItems = store.update;
+export const changeStream = store.changeStream;
+export const valueStream = store.valueStream;
 
-export async function setItems<T extends Partial<LocalStorageShape>>(items: T) {
-  await chrome.storage.local.set(items);
-}
-
-export async function getGuaranteedItems<T extends Partial<LocalStorageShape>>(...keys: (keyof T)[]) {
-  const result = (await chrome.storage.local.get(keys)) as T;
-  let defaultValues: Partial<LocalStorageShape> = {};
-
-  keys.forEach((key) => {
-    if (result[key] === undefined) {
-      // FIXME: I think the compiler is not recognizing the correlation between defaultValues and LOCAL_STORAGE_DEFAULT_VALUES
-      // @ts-ignore
-      defaultValues[key] = LOCAL_STORAGE_DEFAULT_VALUES[key];
-    }
-  });
-
-  if (Object.keys(defaultValues).length > 0) {
-    await chrome.storage.local.set(defaultValues);
-    return { ...result, ...defaultValues };
-  }
-
-  return result;
-}
-
-export async function useOrGetItems<T extends Partial<LocalStorageShape>>(keys: Partial<T> = {}) {
-  const missingValues: (keyof T)[] = [];
-  for (const key in keys) {
-    if (keys[key] === undefined) {
-      missingValues.push(key);
-    }
-  }
-
-  if (missingValues.length > 0) {
-    const defaultValues = await getGuaranteedItems(...missingValues);
-    return { ...keys, ...defaultValues };
-  }
-  return keys;
-}
-
-export async function useOrGetItem<T extends keyof LocalStorageShape>(key: T, value: LocalStorageShape[T] | undefined) {
-  let result: LocalStorageShape[T] | undefined = value;
-  if (result === undefined) {
-    result = (await getGuaranteedItems(key))[key];
-  }
-
-  if (result === undefined) {
-    throw new Error(`useOrGetItem: result for ${key} is undefined`);
-  }
-
-  return result;
-}
+updateItems(async (prev) => {
+  return prev;
+});
