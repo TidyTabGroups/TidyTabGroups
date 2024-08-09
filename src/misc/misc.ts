5adf683456ad9ca68e9b7cb026de0ee210c3823c
Moe Bazzi <bazzimoe3@gmail.com>
2024-08-09 10:38:45 -0600

    Implement Misc.getErrorMessage for getting the string from an unknown error type

--- src/misc/misc.ts
+++ src/misc/misc.ts
@@ -80,3 +80,16 @@ export function serviceWorkerJustWokeUp() {
 }
 
 export const DEFAULT_TAB_GROUP_TITLE = "New Group";
+
+export function getErrorMessage(error: unknown): string {
+  if (error instanceof Error) {
+    return error.message;
+  }
+  if (typeof error === "string") {
+    return error;
+  }
+  if (error && typeof error === "object" && "message" in error && typeof error.message === "string") {
+    return error.message;
+  }
+  return "An unknown error occurred";
+}
