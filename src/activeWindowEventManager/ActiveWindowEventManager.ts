80d37d50514048cfdf6b420ff6a6e32b96b17b87
Moe Bazzi <bazzimoe3@gmail.com>
2024-08-16 18:46:38 -0600

    Improve logger usage in chrome.windows.onFocusChanged

--- src/activeWindowEventManager/ActiveWindowEventManager.ts
+++ src/activeWindowEventManager/ActiveWindowEventManager.ts
@@ -104,15 +104,16 @@ export async function initialize(onError: () => void) {
   });
 
   chrome.windows.onFocusChanged.addListener((windowId: ChromeWindowId) => {
+    const myLogger = logger.createNestedLogger("windows.onFocusChanged");
     queueOperation(
       {
-        name: "onFocusChanged",
+        name: myLogger.getPrefixedMessage("onFocusChanged"),
         operation: async () => {
           // check the up-to-date focused window because it could have changed
           const lastFocusedWindow = (await chrome.windows.getLastFocused()) as ChromeWindowWithId;
           if (windowId === chrome.windows.WINDOW_ID_NONE ? lastFocusedWindow.focused : lastFocusedWindow.id !== windowId) {
-            logger.warn(
-              `onFocusChanged::focused window not up to date - windowId: ${windowId}, lastFocusedWindow: ${lastFocusedWindow.id}, lastFocusedWindow.focused: ${lastFocusedWindow.focused}`
+            myLogger.warn(
+              `focused window not up to date - windowId: ${windowId}, lastFocusedWindow: ${lastFocusedWindow.id}, lastFocusedWindow.focused: ${lastFocusedWindow.focused}`
             );
             return;
           }
