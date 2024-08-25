27f1b797d9a8caad08c738341e0199b5c74030f5
Moe Bazzi <bazzimoe3@gmail.com>
2024-08-25 11:02:02 -0600

    Remove TODO: open the onboarding page

--- src/activeWindowManager/ActiveWindowManager.ts
+++ src/activeWindowManager/ActiveWindowManager.ts
@@ -446,7 +446,6 @@ export async function initialize(onError: (message: string) => void) {
 export async function onInstalled(details: chrome.runtime.InstalledDetails) {
   logger.log(`onInstalled::Extension was installed because of: ${details.reason}`);
   if (details.reason === "install") {
-    // TODO: open the onboarding page
     await ActiveWindowMethods.activateAllWindows();
   } else {
     await ActiveWindowMethods.reactivateAllWindows();
