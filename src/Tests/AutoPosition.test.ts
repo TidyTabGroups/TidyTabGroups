cf690a0e52af6a219e37a81571e2de0b1e20742e
Mohamad Bazzi <mohamadbazzi@Moes-MacBook-Pro-2.local>
2024-10-23 15:12:21 -0400

    Remove Playwright test descriptions

--- src/Tests/AutoPosition.test.ts
+++ src/Tests/AutoPosition.test.ts
@@ -4,104 +4,99 @@ import { test, expect } from "./Fixtures/Fixture";
 
 const DEFAULT_TEST_PAGE_URL = "file://" + __dirname + "/Resources/Pages/DefaultPage.html";
 
-test.describe("Auto Position", () => {
-  test("Focused tab group and tab get repositioned to end of tab bar", async ({
-    chromeProxy,
-    context,
-  }) => {
-    const [targetPage, targetPageTab] =
-      await test.step("Create target page and tab group", async () => {
-        let targetPageTabId: ChromeTabId | undefined;
-        const becomesLoadedPromise = chromeProxy.waitFor(
-          "tabs.onUpdated",
-          async (tabId, changeInfo, tab) => {
-            if (changeInfo.status === "complete" && tab.url === DEFAULT_TEST_PAGE_URL) {
-              targetPageTabId = tabId;
-              return true;
-            }
-            return false;
-          }
-        );
-        const becomesGroupedPromise = chromeProxy.waitFor(
-          "tabs.onUpdated",
-          async (_, changeInfo) => {
-            return (
-              changeInfo.groupId !== undefined &&
-              changeInfo.groupId !== chromeProxy.tabGroups.TAB_GROUP_ID_NONE
-            );
-          }
-        );
-        const page = await context.newPage();
-        await page.goto(DEFAULT_TEST_PAGE_URL);
-        await becomesGroupedPromise;
-        await becomesLoadedPromise;
-
-        if (targetPageTabId === undefined) {
-          throw new Error("targetPageTabId was not set");
-        }
-
-        const targetPageTab = await chromeProxy.tabs.get(targetPageTabId);
-        // Title the tab group to make it easier to identify
-        await chromeProxy.tabGroups.update(targetPageTab.groupId, { title: " * Target * " });
-
-        return [page, targetPageTab];
-      });
-
-    await test.step("Create an active tab group with a tab positioned after the target tab", async () => {
-      const becomesGroupedPromise = chromeProxy.waitFor(
+test("Focused tab group and tab get repositioned to end of tab bar", async ({
+  chromeProxy,
+  context,
+}) => {
+  const [targetPage, targetPageTab] =
+    await test.step("Create target page and tab group", async () => {
+      let targetPageTabId: ChromeTabId | undefined;
+      const becomesLoadedPromise = chromeProxy.waitFor(
         "tabs.onUpdated",
         async (tabId, changeInfo, tab) => {
-          return (
-            changeInfo.groupId !== undefined &&
-            changeInfo.groupId !== chromeProxy.tabGroups.TAB_GROUP_ID_NONE
-          );
-        }
-      );
-      const tab = await chromeProxy.tabs.create({
-        active: true,
-        index: targetPageTab.index + 1,
-        windowId: targetPageTab.windowId,
-      });
-      await becomesGroupedPromise;
-      const tabGroup = await chromeProxy.tabs.group({
-        tabIds: [tab.id],
-        createProperties: { windowId: tab.windowId },
-      });
-      // Title the tab group to make it easier to identify
-      await chromeProxy.tabGroups.update(tabGroup, { title: " * Tab Group After *" });
-    });
-
-    await test.step("Focus target tab group and wait for it and it's tab to be repositioned", async () => {
-      const tabBecomesRepositionedPromise = chromeProxy.waitFor(
-        "tabs.onMoved",
-        async (tabId, moveInfo) => {
-          if (tabId === targetPageTab.id) {
-            const tabs = await chromeProxy.tabs.query({ windowId: targetPageTab.windowId });
-            console.log(`moveInfo.toIndex: ${moveInfo.toIndex}, tabs.length: ${tabs.length}`);
-            return moveInfo.toIndex === tabs.length - 1;
+          if (changeInfo.status === "complete" && tab.url === DEFAULT_TEST_PAGE_URL) {
+            targetPageTabId = tabId;
+            return true;
           }
           return false;
         }
       );
-
-      const tabGroupBecomesRepositionedPromise = chromeProxy.waitFor(
-        "tabGroups.onMoved",
-        async (tabGroup) => {
-          return true;
-        }
-      );
-
-      await targetPage.bringToFront();
-      await targetPage.evaluate(() => {
-        // Need to give a height to the body to make it hoverable for Playwright
-        document.body.style.height = "1px";
+      const becomesGroupedPromise = chromeProxy.waitFor("tabs.onUpdated", async (_, changeInfo) => {
+        return (
+          changeInfo.groupId !== undefined &&
+          changeInfo.groupId !== chromeProxy.tabGroups.TAB_GROUP_ID_NONE
+        );
       });
-      // WARNING: This does not work if your cursor is in the test page.
-      await targetPage.hover("body");
-      await tabBecomesRepositionedPromise;
-      await tabGroupBecomesRepositionedPromise;
+      const page = await context.newPage();
+      await page.goto(DEFAULT_TEST_PAGE_URL);
+      await becomesGroupedPromise;
+      await becomesLoadedPromise;
+
+      if (targetPageTabId === undefined) {
+        throw new Error("targetPageTabId was not set");
+      }
+
+      const targetPageTab = await chromeProxy.tabs.get(targetPageTabId);
+      // Title the tab group to make it easier to identify
+      await chromeProxy.tabGroups.update(targetPageTab.groupId, { title: " * Target * " });
+
+      return [page, targetPageTab];
     });
 
-    expect(true).toBe(true);
+  await test.step("Create an active tab group with a tab positioned after the target tab", async () => {
+    const becomesGroupedPromise = chromeProxy.waitFor(
+      "tabs.onUpdated",
+      async (tabId, changeInfo, tab) => {
+        return (
+          changeInfo.groupId !== undefined &&
+          changeInfo.groupId !== chromeProxy.tabGroups.TAB_GROUP_ID_NONE
+        );
+      }
+    );
+    const tab = await chromeProxy.tabs.create({
+      active: true,
+      index: targetPageTab.index + 1,
+      windowId: targetPageTab.windowId,
+    });
+    await becomesGroupedPromise;
+    const tabGroup = await chromeProxy.tabs.group({
+      tabIds: [tab.id],
+      createProperties: { windowId: tab.windowId },
+    });
+    // Title the tab group to make it easier to identify
+    await chromeProxy.tabGroups.update(tabGroup, { title: " * Tab Group After *" });
   });
+
+  await test.step("Focus target tab group and wait for it and it's tab to be repositioned", async () => {
+    const tabBecomesRepositionedPromise = chromeProxy.waitFor(
+      "tabs.onMoved",
+      async (tabId, moveInfo) => {
+        if (tabId === targetPageTab.id) {
+          const tabs = await chromeProxy.tabs.query({ windowId: targetPageTab.windowId });
+          console.log(`moveInfo.toIndex: ${moveInfo.toIndex}, tabs.length: ${tabs.length}`);
+          return moveInfo.toIndex === tabs.length - 1;
+        }
+        return false;
+      }
+    );
+
+    const tabGroupBecomesRepositionedPromise = chromeProxy.waitFor(
+      "tabGroups.onMoved",
+      async (tabGroup) => {
+        return true;
+      }
+    );
+
+    await targetPage.bringToFront();
+    await targetPage.evaluate(() => {
+      // Need to give a height to the body to make it hoverable for Playwright
+      document.body.style.height = "1px";
+    });
+    // WARNING: This does not work if your cursor is in the test page.
+    await targetPage.hover("body");
+    await tabBecomesRepositionedPromise;
+    await tabGroupBecomesRepositionedPromise;
+  });
+
+  expect(true).toBe(true);
 });
--- src/Tests/autoCollapse.test.ts
+++ src/Tests/autoCollapse.test.ts
@@ -1,86 +1,81 @@
 import Misc from "../Shared/Misc";
 import { test, expect } from "./Fixtures/Fixture";
 
-test.describe("Auto Collapse", () => {
-  test("Previous active tab group becomes collapsed", async ({ chromeProxy }) => {
-    const activeTabGroupTitle = " * Active * ";
-    const activeTab = await test.step("Create active tab group", async () => {
-      // Create active tab
-      const becomesGroupedPromise = chromeProxy.waitFor(
-        "tabs.onUpdated",
-        async (tabId, changeInfo, tab) => {
-          return (
-            tabId === activeTab.id &&
-            changeInfo.groupId !== undefined &&
-            changeInfo.groupId !== chromeProxy.tabGroups.TAB_GROUP_ID_NONE
-          );
-        }
-      );
-      let activeTab = await chromeProxy.tabs.create({ pinned: false, active: true });
-      await becomesGroupedPromise;
-      activeTab = await chromeProxy.tabs.get(activeTab.id);
-
-      // Update Title for easier debugging
-      await chromeProxy.tabGroups.update(activeTab.groupId, { title: activeTabGroupTitle });
-
-      return activeTab;
-    });
-
-    const nonActiveTabGroupTitle = " * Non-Active * ";
-    const nonActiveTab = await test.step("Create non-active tab group", async () => {
-      //  Create non-active tab
-      const becomesGroupedPromise = chromeProxy.waitFor(
-        "tabs.onUpdated",
-        async (tabId, changeInfo, tab) => {
-          return tabId === nonActiveTab.id && changeInfo.groupId === activeTab.groupId;
-        }
-      );
-      let nonActiveTab = await chromeProxy.tabs.create({
-        pinned: false,
-        windowId: activeTab.windowId,
-        active: false,
-      });
-      await becomesGroupedPromise;
-      nonActiveTab = await chromeProxy.tabs.get(nonActiveTab.id);
-
-      // Create non-active tab group
-      const becomesCollapsedPromise = chromeProxy.waitFor(
-        "tabGroups.onUpdated",
-        async (tabGroup) => {
-          return tabGroup.id === nonActiveTabGroupId && tabGroup.collapsed === true;
-        }
-      );
-      const nonActiveTabGroupId = await chromeProxy.tabs.group({
-        tabIds: [nonActiveTab.id],
-        createProperties: { windowId: nonActiveTab.windowId },
-      });
-      nonActiveTab = await chromeProxy.tabs.get(nonActiveTab.id);
-      let nonActiveTabGroup = await chromeProxy.tabGroups.update(nonActiveTabGroupId, {
-        // Update title for easier debugging
-        title: nonActiveTabGroupTitle,
-      });
-
-      if (!nonActiveTabGroup.collapsed) {
-        await becomesCollapsedPromise;
-        nonActiveTabGroup = await chromeProxy.tabGroups.get(nonActiveTabGroupId);
-        if (!nonActiveTabGroup.collapsed) {
-          throw new Error("nonActiveTabGroup is not collapsed");
-        }
-      }
-
-      return nonActiveTab;
-    });
-
-    // Expand non-active tab group and wait for previous active tab group to become collapsed
-    const previousActiveTabGroupBecomesCollapsedPromise = chromeProxy.waitFor(
-      "tabGroups.onUpdated",
-      async (tabGroup) => {
-        return tabGroup.id === activeTab.groupId && tabGroup.collapsed === true;
+test("Previous active tab group becomes collapsed", async ({ chromeProxy }) => {
+  const activeTabGroupTitle = " * Active * ";
+  const activeTab = await test.step("Create active tab group", async () => {
+    // Create active tab
+    const becomesGroupedPromise = chromeProxy.waitFor(
+      "tabs.onUpdated",
+      async (tabId, changeInfo, tab) => {
+        return (
+          tabId === activeTab.id &&
+          changeInfo.groupId !== undefined &&
+          changeInfo.groupId !== chromeProxy.tabGroups.TAB_GROUP_ID_NONE
+        );
       }
     );
-    await chromeProxy.tabGroups.update(nonActiveTab.groupId, { collapsed: false });
-    await previousActiveTabGroupBecomesCollapsedPromise;
+    let activeTab = await chromeProxy.tabs.create({ pinned: false, active: true });
+    await becomesGroupedPromise;
+    activeTab = await chromeProxy.tabs.get(activeTab.id);
 
-    expect(true).toBe(true);
+    // Update Title for easier debugging
+    await chromeProxy.tabGroups.update(activeTab.groupId, { title: activeTabGroupTitle });
+
+    return activeTab;
   });
+
+  const nonActiveTabGroupTitle = " * Non-Active * ";
+  const nonActiveTab = await test.step("Create non-active tab group", async () => {
+    //  Create non-active tab
+    const becomesGroupedPromise = chromeProxy.waitFor(
+      "tabs.onUpdated",
+      async (tabId, changeInfo, tab) => {
+        return tabId === nonActiveTab.id && changeInfo.groupId === activeTab.groupId;
+      }
+    );
+    let nonActiveTab = await chromeProxy.tabs.create({
+      pinned: false,
+      windowId: activeTab.windowId,
+      active: false,
+    });
+    await becomesGroupedPromise;
+    nonActiveTab = await chromeProxy.tabs.get(nonActiveTab.id);
+
+    // Create non-active tab group
+    const becomesCollapsedPromise = chromeProxy.waitFor("tabGroups.onUpdated", async (tabGroup) => {
+      return tabGroup.id === nonActiveTabGroupId && tabGroup.collapsed === true;
+    });
+    const nonActiveTabGroupId = await chromeProxy.tabs.group({
+      tabIds: [nonActiveTab.id],
+      createProperties: { windowId: nonActiveTab.windowId },
+    });
+    nonActiveTab = await chromeProxy.tabs.get(nonActiveTab.id);
+    let nonActiveTabGroup = await chromeProxy.tabGroups.update(nonActiveTabGroupId, {
+      // Update title for easier debugging
+      title: nonActiveTabGroupTitle,
+    });
+
+    if (!nonActiveTabGroup.collapsed) {
+      await becomesCollapsedPromise;
+      nonActiveTabGroup = await chromeProxy.tabGroups.get(nonActiveTabGroupId);
+      if (!nonActiveTabGroup.collapsed) {
+        throw new Error("nonActiveTabGroup is not collapsed");
+      }
+    }
+
+    return nonActiveTab;
+  });
+
+  // Expand non-active tab group and wait for previous active tab group to become collapsed
+  const previousActiveTabGroupBecomesCollapsedPromise = chromeProxy.waitFor(
+    "tabGroups.onUpdated",
+    async (tabGroup) => {
+      return tabGroup.id === activeTab.groupId && tabGroup.collapsed === true;
+    }
+  );
+  await chromeProxy.tabGroups.update(nonActiveTab.groupId, { collapsed: false });
+  await previousActiveTabGroupBecomesCollapsedPromise;
+
+  expect(true).toBe(true);
 });
